d90fd54b80583e0c629db3f11b8b4779ff8f95a7
RAFSuNX <128445052+RAFSuNX@users.noreply.github.com>
2025-04-25 17:47:58 +0600

    Update imageData.js

--- jsfiles/imageData.js
+++ jsfiles/imageData.js
@@ -1,24 +1,10 @@
 // Image configuration data
 export const imageData = [
   {
-    image: "/images/image2.jpg",
-    styles: `
-      #main {
-        background-image: url('./images/image2.jpg');
-      }
-      :root {
-        --accent: #011126;
-        --text: #99C8F2;
-        --glass: rgb(27, 65, 89, 0.1);
-        --x: #4681A6;
-      }            
-    `,
-  },
-  {
     image: "https://i.postimg.cc/PxF5M9jY/image3.jpg",
     styles: `
       #main {
-        background-image: url(https://i.postimg.cc/PxF5M9jY/image3.jpg);
+        background-image: url('https://i.postimg.cc/PxF5M9jY/image3.jpg');
       }
       :root {
         --accent: #000000;
